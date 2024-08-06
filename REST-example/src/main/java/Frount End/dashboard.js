28d99fefcb5184c3ef4de8b9366cdf6d27b68313
Atharva Kshirsagar <atharvakshirsagar1987@gmail.com>
2024-08-06 11:25:49 +0530

    HTML front end removed

--- REST-example/src/main/java/Frount End/dashboard.js
+++
@@ -1,7 +0,0 @@
-// templates.js
-
-var templates = {
-    addCustomerTemplate: '<div><h2>Add New Customer</h2><form id="addCustomerForm"><label>Customer ID:</label><input type="text" data-bind="value: customerId"><button type="submit">Submit</button></form></div>',
-    editCustomerTemplate: '<div><h2>Edit Existing Customer</h2><!-- Form and fields for editing customer details --></div>',
-    calculateBillTemplate: '<div><h2>Calculate Bill Amount</h2><!-- Form and fields for calculating bill amount --></div>'
-};
